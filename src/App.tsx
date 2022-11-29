a0690d0fa56e2f2a7e7852e93e47849f28647368
Vadym Mamchur <vmchr344@gmail.com>
2022-11-29 13:38:12 +0200

    feat: mobile header has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import menu from './menu.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <header className='header'>
+        <img src={logo} alt="" className="logo" />
+        <a href="#menu" className="header__menu">
+          <img src={menu} alt="menu" className="icon icon--menu" />
         </a>
       </header>
     </div>
